b0ab97acdbe0b038d3280c06fbfa360c777be299
DESKTOP-2D9MENH\woong <woongsu0614@gmail.com>
2021-01-18 01:01:27 +0900

    lec 10 : error occur

--- index.js
+++ index.js
@@ -22,18 +22,18 @@ mongoose.connect(config.mongoURI, {
 
 
 app.get('/', (req, res) => res.send('hello world 새해복 '))
+
 app.post('/register', (req, res) => { 
     //회원가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다
-    const user = new User(req.body)
-    user.save((err, doc) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
         if(err) return res.json({success:false, err})
-        //status 200 : 성공했다는 뜻
+        
         return res.status(200).json({
             success: true
         })
     })
-    
 })
 
 app.listen(port, () => console.log('example app is listening on port ${port}!'))
